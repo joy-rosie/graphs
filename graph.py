661626e12d6c283222c340fbb50afb57e7bb99ce
Alix <joytolia@hotmail.com>
2019-01-26 16:13:10 +0000

    nice version of graph

--- graph.py
+++ graph.py
@@ -1,16 +1,32 @@
 import pandas as pd
 import numpy as np
 import networkx as nx
+from itertools import chain
+
+
+def list_validator(input_data):
+    output = input_data
+    if not isinstance(output, list):
+        output = [output]
+    return output
+
+
+def make_vertex_index(input_list=None):
+    if input_list is None:
+        input_list = list()
+    return pd.Index(input_list, name='vertex_index')
 
 
 class Graph:
 
     _vdf = None
-    _edf = None
+    _edges = None
+    _defaults_for_attributes = None
 
     def initialise(self):
-        self._vdf = pd.DataFrame()
-        self._edf = pd.DataFrame()
+        self._vdf = pd.DataFrame(index=make_vertex_index())
+        self._edges = set()
+        self._defaults_for_attributes = dict()
 
     def __init__(self, vertices=None, vertex_attributes=None):
         self.initialise()
@@ -21,19 +37,22 @@ class Graph:
     def vdf(self):
         return self._vdf
 
-    @vdf.setter
-    def vdf(self, value):
-        self._vdf = value
-        self._vdf = self._vdf.sort_index()
-        self.update_edf_from_vdf()
+    def make_edf_from_egdes(self):
+        vertices = self.vertices
+        edf = pd.DataFrame(0, index=vertices, columns=vertices)
+        edges = [(edge[0], edge[1], 1) for edge in self.edges]
+        temp_edf = pd.DataFrame(edges, columns=['vertex_1', 'vertex_2', 'edge'])\
+            .pivot('vertex_1', 'vertex_2', 'edge')\
+            .fillna(0)
+        edf.update(temp_edf)
+        return edf
 
     @property
     def edf(self):
-        return self._edf
-
-    @edf.setter
-    def edf(self, value):
-        self._edf = value.astype('int64')
+        edf = self.make_edf_from_egdes()
+        edf = edf + edf.T
+        edf[edf > 0] = 1
+        return edf
 
     @property
     def vertices(self):
@@ -41,7 +60,7 @@ class Graph:
 
     @property
     def edges(self):
-        return 'Not implemented'
+        return self._edges
 
     def add_vertices(self, vertices=None, n_vertices=None):
         if n_vertices is not None:
@@ -49,13 +68,11 @@ class Graph:
             vertices = list(range(start+1, start + n_vertices + 1))
 
         if vertices is not None:
-            if not isinstance(vertices, list):
-                vertices = [vertices]
+            vertices = list_validator(vertices)
 
             new_index = pd.Index(vertices)
-
-            self.vdf = pd.concat([self.vdf, pd.DataFrame(index=new_index[~new_index.isin(self.vdf.index)])],
-                                 axis=1)
+            vertex_index = make_vertex_index(new_index[~new_index.isin(self.vdf.index)])
+            self._vdf = pd.concat([self.vdf, pd.DataFrame(index=vertex_index)], axis=1).sort_index()
 
     @property
     def vertex_attributes(self):
@@ -64,17 +81,20 @@ class Graph:
     def add_vertex_attributes(self, vertex_attributes=None):
         if vertex_attributes is not None:
             if not isinstance(vertex_attributes, dict):
-                if not isinstance(vertex_attributes, list):
-                    vertex_attributes = [vertex_attributes]
+                vertex_attributes = list_validator(vertex_attributes)
                 vertex_attributes = {item: None for item in vertex_attributes}
             # vertex_attributes = {key: value for key, value in vertex_attributes.items()
             #                      if key not in self.vdf.columns}
-            self.vdf = self.vdf.assign(**vertex_attributes)
+            self._vdf = self.vdf.assign(**vertex_attributes)
 
-    def update_edf_from_vdf(self):
-        new_edf = pd.DataFrame(0, index=self.vdf.index, columns=self.vdf.index)
-        new_edf.update(self.edf)
-        self.edf = new_edf
+    def add_edges(self, edges=None):
+        if edges is not None:
+            edges = list_validator(edges)
+            if not isinstance(edges[0], (list, tuple)):
+                edges = [edges]
+            edges = [(edge[0], edge[1]) for edge in edges]
+            self.add_vertices(vertices=list(chain.from_iterable(edges)))
+            self._edges.update(edges)
 
     def __str__(self):
         return 'Graph - Vertices: ' + str(self.vertices)
--- test_graph.py
+++ test_graph.py
@@ -3,6 +3,7 @@ import pandas as pd
 from collections import Counter
 
 from graph import Graph
+from graph import make_vertex_index
 
 
 def compare_lists_unordered(list_1, list_2):
@@ -18,32 +19,32 @@ def test_add_vertices():
     g = Graph()
     g.add_vertices()
     assert g.vertices == []
-    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame())
+    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame(index=make_vertex_index()))
     g.add_vertices([0])
-    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame(index=[0]))
+    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame(index=make_vertex_index([0])))
     assert g.vertices == [0]
     g.add_vertices(1)
-    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame(index=[0, 1]))
+    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame(index=make_vertex_index([0, 1])))
     assert g.vertices == [0, 1]
     g.add_vertices([3, 2])
-    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame(index=[0, 1, 2, 3]))
+    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame(index=make_vertex_index([0, 1, 2, 3])))
     assert g.vertices == [0, 1, 2, 3]
     g.add_vertices(3)
-    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame(index=[0, 1, 2, 3]))
+    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame(index=make_vertex_index([0, 1, 2, 3])))
     assert g.vertices == [0, 1, 2, 3]
 
     g = Graph(vertices=[0, 1, 2])
-    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame(index=[0, 1, 2]))
+    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame(index=make_vertex_index([0, 1, 2])))
     assert g.vertices == [0, 1, 2]
 
     g = Graph()
     g.add_vertices(n_vertices=3)
-    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame(index=[0, 1, 2]))
+    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame(index=make_vertex_index([0, 1, 2])))
     assert g.vertices == [0, 1, 2]
 
     g = Graph(vertices=[0])
     g.add_vertices(n_vertices=3)
-    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame(index=[0, 1, 2, 3]))
+    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame(index=make_vertex_index([0, 1, 2, 3])))
     assert g.vertices == [0, 1, 2, 3]
 
 
@@ -59,36 +60,33 @@ def test_add_vertex_attributes():
     g = Graph(vertices=[0, 1])
     g.add_vertex_attributes(vertex_attributes={'name': 'test', 'level': 0})
     assert compare_lists_unordered(g.vertex_attributes, ['name', 'level'])
-    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame({'name': ['test']*2, 'level': [0]*2}, index=[0, 1]))
+    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame({'name': ['test']*2, 'level': [0]*2},
+                                                      index=make_vertex_index([0, 1])))
     g.add_vertex_attributes(vertex_attributes={'name': 'change_test', 'new_level': 1})
     pd.testing.assert_frame_equal(g.vdf, pd.DataFrame({'name': ['change_test']*2, 'level': [0]*2, 'new_level': [1]*2},
-                                                      index=[0, 1]))
+                                                      index=make_vertex_index([0, 1])))
 
 
 def test_add_vertices_with_vertex_attributes():
     g = Graph(vertices=[0, 1], vertex_attributes=['name', 'level'])
     g.add_vertices(vertices=[2, 3])
-    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame({'name': [None]*4, 'level': [None]*4}, index=[0, 1, 2, 3]))
+    pd.testing.assert_frame_equal(g.vdf, pd.DataFrame({'name': [None]*4, 'level': [None]*4},
+                                                      index=make_vertex_index([0, 1, 2, 3])))
 
 
-def test_update_edf_from_vdf():
-    g = Graph()
-    g._vdf = pd.DataFrame(index=[0, 1])
-    g.update_edf_from_vdf()
-    pd.testing.assert_frame_equal(g.edf, pd.DataFrame({0: [0]*2, 1: [0]*2}, index=[0, 1]))
-
-    g._vdf = pd.DataFrame(index=[0, 1, 2, 3])
-    g.update_edf_from_vdf()
-    pd.testing.assert_frame_equal(g.edf, pd.DataFrame({0: [0]*4, 1: [0]*4, 2: [0]*4, 3: [0]*4}, index=[0, 1, 2, 3]))
-
-    g._edf = pd.DataFrame({0: [0, 1], 1: [1, 0]}, index=[0, 1])
-    g.update_edf_from_vdf()
-    pd.testing.assert_frame_equal(g.edf, pd.DataFrame({0: [0, 1] + [0]*2, 1: [1, 0] + [0]*2, 2: [0]*4, 3: [0]*4},
-                                                      index=[0, 1, 2, 3]))
+def test_add_edges():
+    g = Graph(vertices=0)
+    g.add_edges(edges=[0, 0])
+    pd.testing.assert_frame_equal(g.edf, pd.DataFrame(1, index=[0], columns=[0]))
+    g.add_edges(edges=(1, 0))
+    pd.testing.assert_frame_equal(g.edf, pd.DataFrame({0: [1, 1], 1: [1, 0]}, index=[0, 1], columns=[0, 1]))
+    assert g.vertices == [0, 1]
+    g.add_edges(edges=[(0, 1), [1, 1]])
+    pd.testing.assert_frame_equal(g.edf, pd.DataFrame({0: [1, 1], 1: [1, 1]}, index=[0, 1], columns=[0, 1]))
 
 
-def test_vdf():
-
-    g = Graph(vertices=[0, 1], vertex_attributes='name')
-    g.vdf.loc[0, 'name'] = 'first'
-    assert g._vdf.loc[0, 'name'] == 'first' and g.vdf.loc[0, 'name'] == 'first'
+# def test_edf():
+#
+#     g = Graph(vertices=[0, 1])
+#     g.edf.loc[0, 1] = 1
+#     assert g._edf.loc[0, 1] == 1 and g.edf.loc[0, 1] == 1 and g._edf.loc[1, 0] == 1 and g.edf.loc[1, 0] == 1
